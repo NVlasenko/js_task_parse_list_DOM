cbd3129310f884d39c3cb3a482ab8c4c112b4bfd
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2025-03-06 12:01:50 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const employeeList = document.querySelectorAll('ul li');
+
+  function parseSalary(salaryString) {
+    return parseFloat(salaryString.replace(/[^0-9.-]+/g, ''));
+  }
+
+  function getEmployees(list) {
+    return Array.from(list).map((li) => {
+      return {
+        name: li.textContent.trim(),
+        position: li.getAttribute('data-position'),
+        salary: parseSalary(li.getAttribute('data-salary')),
+        age: parseInt(li.getAttribute('data-age'), 10),
+      };
+    });
+  }
+
+  function sortList(list) {
+    const employees = getEmployees(list);
+
+    return employees.sort((a, b) => b.salary - a.salary);
+  }
+
+  sortList(employeeList);
+  getEmployees(employeeList);
+});
